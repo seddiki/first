02a2b3d5c3ea64d5bc13b1fd78f8ba0d23eab26a
seddiki <seddiki_-_seif@hotmail.fr>
2015-04-22 11:54:07 +0100

    first commit

---
+++ Documents/apprails/app1/first/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+First::Application.config.secret_token = 'aef1f99cc6c31094123718183f3d93a468efa4b87d8c3d0dc3fc519b154974a0c0d2669ada15702bda7385361f4761bfb2831bdf4310951ccd50916e3f4524bc'
